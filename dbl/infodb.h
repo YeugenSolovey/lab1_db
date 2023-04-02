775884a8ec5aac57ea3b5e82759c73bce402fbb5
YeugenSolovey <116490331+YeugenSolovey@users.noreply.github.com>
2023-04-02 15:47:54 +0300

    Create infodb.h

---
+++ dbl/infodb.h
@@ -0,0 +1,9 @@
+//#pragma once
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//#include <Windows.h>
+//#include "func_game.h"
+//#include "func_genre.h"
+//
+//void info(Genre* d, int n);
