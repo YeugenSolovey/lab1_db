ea39f861ed3ea392593d156562b67dd35d6432ba
YeugenSolovey <116490331+YeugenSolovey@users.noreply.github.com>
2023-04-02 15:46:52 +0300

    Delete infodb.h

--- dbl/infodb.h
+++
@@ -1,9 +0,0 @@
-//#pragma once
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <Windows.h>
-//#include "func_game.h"
-//#include "func_genre.h"
-//
-//void info(Genre* d, int n);
